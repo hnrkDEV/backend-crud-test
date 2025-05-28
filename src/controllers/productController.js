c3dc37e58cb477d5a18cf1ea116ec4d8f3236ce6
João Henrique <jhenrique.caval@hotmail.com>
2025-05-27 23:43:59 -0300

    feat: CRUD controller finished

---
+++ src/controllers/productController.js
@@ -0,0 +1,58 @@
+const Product = require('../models/productModel');
+const logger = require('../config/logger');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createProduct = catchAsync(async(req, res, next) => {
+    const newProduct = await Product.create(req.body);
+    logger.info(`product ${newProduct.name} created successfully!`);
+
+    res.status(201).json({
+    status: 'success',
+    data: {
+      product: newProduct
+    }
+  });
+  });
+
+exports.listProducts = catchAsync(async(req, res, next) => {
+  const products = await Product.find();
+
+  logger.info(`found ${products.length} products.`)
+  res.status(200).json({
+    status: 'success',
+    data: {
+      products
+    }
+  });
+});
+
+exports.deleteProduct = catchAsync(async(req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Product not found'
+    });
+  };
+  
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
+exports.updateProduct = catchAsync(async(req, res, next) => {
+  const updatedProd = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators: true})
+
+  if(!updatedProd)  {
+    logger.error(`Product with id ${updatedProd._id} not found`);
+    return res.status(404).json({status: 'fail', message: 'product not found!'});
+  };
+
+    logger.info('Product updated successfully!')
+  res.status(200).json({
+    status: 'success',
+    data: updatedProd
+  });
+});
